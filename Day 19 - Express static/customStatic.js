8da7e6b6424c3a83d860e3798f4017ea6d7d7e7c
TikoSolyan731 <tikosolyan731@gmail.com>
2021-03-26 22:56:56 +0400

    Fixed an await issue

--- Day 19 - Express static/customStatic.js
+++ Day 19 - Express static/customStatic.js
@@ -4,7 +4,7 @@ const {existsSync} = require('fs');
 module.exports = (root) => {
     return async (req, res, next) => {
         const p = path.resolve(`${root}${req.path}`);
-        const exists = await existsSync(p);
+        const exists = existsSync(p);
 
         if (exists) {
             res.sendFile(p);
